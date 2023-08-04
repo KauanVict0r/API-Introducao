bcfa0dd12bfd6da16bd005b0cf721f304548bd5f
KauanVict0r <132668839+KauanVict0r@users.noreply.github.com>
2023-08-04 11:02:43 +0000

    novo

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import express from "express";      // Requisição do pacote do express
+//index.js
+import dotenv from "dotenv";
+import express from "express";  
+import { selectUsuarios } from "./bd.js";
+dotenv.config();
+
+    // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
@@ -7,6 +13,16 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
     nome: "Kauan_Victor_dos_Santos",      // Substitua pelo seu nome
   });
   console.log("Rota / solicitada");
+});
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
