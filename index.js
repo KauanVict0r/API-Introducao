49bc6d37f559e84b3d67ff2cf7bc7d0b055baa2c
KauanVict0r <132668839+KauanVict0r@users.noreply.github.com>
2023-08-11 11:19:13 +0000

    id

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota / solicitada");
 });
 
-//index.js
+
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
 app.get("/usuario/:id", async (req, res) => {
   console.log("Rota GET /usuario solicitada");
   try {
@@ -30,16 +35,3 @@ app.get("/usuario/:id", async (req, res) => {
   }
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
-});
-
-app.get("/usuarios", async (req, res) => {
-  console.log("Rota GET/usuarios solicitada");
-  try {
-    const usuarios = await selectUsuarios();
-    res.json(usuarios);
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
