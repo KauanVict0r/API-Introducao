aa05b6ba49a4453f079146115c84a191373de676
KauanVict0r <132668839+KauanVict0r@users.noreply.github.com>
2023-08-11 11:05:48 +0000

    salvar

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";  
+//index.js
+import { selectUsuarios, selectUsuario } from "./bd.js";
 import { selectUsuarios } from "./bd.js";
+
+//index.js
 dotenv.config();
 
     // Requisição do pacote do express
@@ -15,6 +19,22 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota / solicitada");
 });
 
+//index.js
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
 app.get("/usuarios", async (req, res) => {
   console.log("Rota GET/usuarios solicitada");
   try {
@@ -23,8 +43,4 @@ app.get("/usuarios", async (req, res) => {
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
-});
-
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
 });
